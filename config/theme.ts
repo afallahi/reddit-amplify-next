9e60a7b6acff3ad5ef3de45017e72c984430986e
Afshin <fallahi_a@yahoo.com>
2023-10-10 23:41:21 -0400

    theme

--- config/theme.ts
+++ config/theme.ts
@@ -1,17 +1,40 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
-// Create a theme instance.
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 const theme = createTheme({
-palette: {
-   primary: {
-      main: '#556cd6',
-   },
-   secondary: {
-     main: '#19857b',
-   },
-   error: {
-   main: red.A400,
-   },
+  palette: {
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
+    background: {
+      paper: "#fff",
+      default: "#fafafa",
+    },
+    primary: {
+      light: "#7986cb",
+      main: "#3f51b5",
+      dark: "#303f9f",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff4081",
+      main: "#f50057",
+      dark: "#c51162",
+      contrastText: "#fff",
+    },
+    error: {
+      light: "#e57373",
+      main: "#f44336",
+      dark: "#d32f2f",
+      contrastText: "#fff",
+    },
+    text: {
+      primary: "rgba(0, 0, 0, 0.87)",
+      secondary: "rgba(0, 0, 0, 0.54)",
+      disabled: "rgba(0, 0, 0, 0.38)",
+    },
   },
 });
-export default theme;
+export default responsiveFontSizes(theme);
+
+
+
